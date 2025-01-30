771b98d64c054d799c9019ef7057f97437890c23
FMonteci <fmonteci@fen.uchile.cl>
2025-01-30 10:44:32 -0300

    Implementada función para agregar amigos

--- app.js
+++ app.js
@@ -1,1 +1,22 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+//creación de variables
+let listadoAmigos=[];
+
+//función que añade amigos al array, valida que no sea vacío
+function agregarAmigo() {
+let inputAmigo =""
+inputAmigo=document.getElementById("amigo").value;
+if (inputAmigo==""){
+    alert("Por favor, inserte un nombre");
+} else {
+    listadoAmigos.push(inputAmigo);
+    document.getElementById("amigo").value="";
+}
+
+console.log(listadoAmigos)
+}
+
+function actualizarAmigo(){
+
+
+}
